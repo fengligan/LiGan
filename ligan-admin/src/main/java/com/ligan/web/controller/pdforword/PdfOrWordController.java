4fb76d7dbfc07a554b3e72185a0c4c73457c9d40
fengligan <fenglg>
2021-08-11 17:44:00 +0800

    提交测试

--- ligan-admin/src/main/java/com/ligan/web/controller/pdforword/PdfOrWordController.java
+++ ligan-admin/src/main/java/com/ligan/web/controller/pdforword/PdfOrWordController.java
@@ -1,11 +1,16 @@
 package com.ligan.web.controller.pdforword;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @program: ligan
  * @description: pdf及word操作
  * @author: fenglg
  * @create: 2021-08-11 17:34
  **/
+@RequestMapping("/word")
+@RestController
 public class PdfOrWordController {
 
 
