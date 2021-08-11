560a7082cd4f5a41c2c4e2ad07651316b9058b28
fengligan <fenglg>
2021-08-11 17:35:23 +0800

    初始化项目

---
+++ ligan-admin/src/main/java/com/ligan/web/controller/pdforword/PdfOrWordController.java
@@ -0,0 +1,10 @@
+package com.ligan.web.controller.pdforword;
+
+/**
+ * @program: ligan
+ * @description: pdf及word操作
+ * @author: fenglg
+ * @create: 2021-08-11 17:34
+ **/
+public class PdfOrWordController {
+}
